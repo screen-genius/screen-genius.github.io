79e7d0b49c59a141c34196fc314fd6a352d63cec
Shamim Imtiaz <km_si@ymail.com>
2021-01-23 15:17:12 -0500

    functional api with results name,url and locations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,36 @@
-console.log("i work");
+var movieName="friends";
+var apiName="https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term="+movieName;
 
-let searchNAme ="James";
-let secondSearchName = "Bond";
-
-fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=James%20Bond", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "2bbe3f6662msh6816e85f5b1dd27p1e0fe8jsncb2c41fb7a72",
-		"x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com"
-	}
+fetch(apiName, {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-key": "2bbe3f6662msh6816e85f5b1dd27p1e0fe8jsncb2c41fb7a72",
+        "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com"
+    }
 })
 .then(response => {
-	
-	return response.json();
-
+    console.log(response);
+    return response.json();
 })
 .then(function(data){
-    displayData(data);
+    displayMovieAttr(data);
 })
 .catch(err => {
-	console.error(err);
-	
-})
+    console.error(err);
+});
 
-function displayData(data){
-    console.log("i work 2");
-    console.log(data);
-}
-/*var apiUrl ='https://api.openweathermap.org/data/2.5/weather?q='+usrChnCity+'&units=imperial&APPID=a812584800c73c0eb9e12320bd5b45ea';
-    fetch(apiUrl)
-        .then(function(response) {   //callback function
-        return response.json(); //response converted to json
-        })
-        .then(function(data) {  //
-        displayCrntWeather(data);
-        });*/
+//displays Movie data
+   function displayMovieAttr(data){
+           
+            var availableAt="";
+            var resultName ="";
+            var resultPicture=""
+            resultName=data.results[0].name;
+            resultPicture = data.results[0].picture;
+            availableAt=data.results[0].locations[0].display_name;
+            console.log(availableAt);
+            console.log(resultPicture);
+            console.log(resultName);
+
+   } 
+        
